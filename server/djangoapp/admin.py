553f1a69e115d961c825ef4adb5550cf12861f1d
Maximo Cozzeti <cozzetimaximo@yahoo.com>
2022-04-23 10:19:10 -0300

    coso

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,23 @@
+from pyexpat import model
 from django.contrib import admin
+
+from .models import CarMake, CarModel
 # from .models import related models
 
 
 # Register your models here.
 
 # CarModelInline class
-
+class CarModelInline(admin.StackedInline):
+    model = CarModel
 # CarModelAdmin class
-
+class CarModelAdmin(admin.ModelAdmin):
+    model = CarModel
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    model = CarModel
+    inlines = [CarModelInline]
 
 # Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
