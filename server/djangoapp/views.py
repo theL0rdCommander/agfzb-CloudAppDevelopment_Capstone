380acd4d30c4ed74d1ae29631b0a3464a35de553
Maximo Cozzeti <cozzetimaximo@yahoo.com>
2022-05-02 23:28:38 -0300

    Create a dealer details/reviews page
    as the title says.

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -10,7 +10,7 @@ from datetime import datetime
 import logging
 import json
 
-from .restapis import get_dealers_from_cf
+from .restapis import get_dealers_from_cf, get_dealer_reviews_from_cf
 #from server import djangoapp
 
 # Get an instance of a logger
@@ -108,7 +108,10 @@ def get_dealerships(request):
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
 def get_dealer_details(request, dealer_id):
-    pass
+    context = {}
+    dealer_reviews = get_dealer_reviews_from_cf(request.url, dealerId=dealer_id)
+    context['dealer_reviews'] = dealer_reviews
+    render(request, 'djangoapp/dealer_details.html', context)
 
 # Create a `add_review` view to submit a review
 def add_review(request, dealer_id):
